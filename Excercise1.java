ea1d3384b755108269bc8ca752a1f52b9fe7f4c9
Rodd Talebi <roddtalebi@gmail.com>
2015-01-08 16:45:12 -0800

    new script

---
+++ Excercise1.java
@@ -0,0 +1,16 @@
+
+public class Excercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//how about we practice some java
+
+		// test test
+
+		int x = 5;
+
+		System.out.println("Hello");
+		System.out.println("5 is equal to " + x);
+	}
+
+}
